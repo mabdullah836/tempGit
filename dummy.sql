9326588806b7110a5165c69c0174fef5e14b8791
mabdullah836 <mabdullah8362@gmail.com>
2024-11-08 17:52:12 +0500

    testing

---
+++ dummy.sql
@@ -0,0 +1,15 @@
+WITH UserRankings AS (
+    SELECT 
+        user_id,
+        user_name,
+        ROW_NUMBER() OVER (ORDER BY user_score DESC) AS rank
+    FROM 
+        users
+)
+SELECT 
+    user_id,
+    user_name
+FROM 
+    UserRankings
+WHERE 
+    rank <= 5;
